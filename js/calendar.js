8455b4a977a7118ae28280945244783de6b13240
Vinicius Santana <santanaviny@gmail.com>
2017-11-10 00:32:53 -0800

    MVP

--- js/calendar.js
+++ js/calendar.js
@@ -37,6 +37,19 @@ Calendar.prototype.renderInit = function() {
       this.updateMonth(this.month, this.year);
     }
   });
+
+
+  el.addEventListener('click', e => {
+    if (e.target.id) {
+      var launch = this.launches[e.target.id];
+      if (launch) {
+        window.alert(`
+          rocket: ${launch.rocket.name}
+          location: ${launch.location.name}
+        `)
+      }
+    }
+  })
 }
 
 Calendar.prototype.getMainTemplate =  function() {
@@ -65,7 +78,6 @@ Calendar.prototype.getMainTemplate =  function() {
   return mainTemplate;
 }
 
-
 Calendar.prototype.fetchMonthEvents = function(month, year) {
   const startMonth = (month < 10) ? '0' + month : '' + month;
   const endMonth = (month === 12) ? '01' : ((month + 1) < 10) ? '0' + (month + 1) : '' + (month + 1);
@@ -75,17 +87,31 @@ Calendar.prototype.fetchMonthEvents = function(month, year) {
   const monthEvents = {};
   const self = this;
 
+  // TODO: memoize fetch result for a given month
+
   fetch(`https://launchlibrary.net/1.2/launch/${startYear}-${startMonth}-01/${endYear}-${endMonth}-01`)
     .then(data => data.json())
     .then((data) => {
       data.launches.forEach((launch) => {
-        let launchDay = new Date(launch.windowstart).getDate();
-        let launchMonth = new Date(launch.windowstart).getMonth() + 1;
-        let launchYear = new Date(launch.windowstart).getFullYear();
-        var launchEl = el.querySelector("[data-day='" + launchDay + "'][data-month='" + launchMonth + "'][data-year='" + launchYear + "']");
-        launchEl.style.color = 'red';
-        console.log(launchEl);
-        this.launches[launchDay + '/' + launchMonth + '/' + launchYear] = launch;
+        const launchDate = new Date(launch.windowstart);
+        const launchDay = launchDate.getDate();
+        const launchMonth = launchDate.getMonth() + 1;
+        const launchYear = launchDate.getFullYear();
+        const launchEl = el.querySelector("[data-day='" + launchDay + "'][data-month='" + launchMonth + "'][data-year='" + launchYear + "']");
+        if (launchEl) {
+            if(!launchEl.className.includes('hasLaunch')){
+              launchEl.className += ' hasLaunch';
+            }
+            const launchElHeader = launchEl.querySelector('header');
+            const launchElMain = launchEl.querySelector('main');
+            const launchLabel = document.createElement('div');
+            const launchLabelText = document.createTextNode(`${launch.name}`);
+            launchLabel.setAttribute('id', launch.id);
+            launchLabel.setAttribute('class', 'launch');
+            launchLabel.appendChild(launchLabelText);
+            launchElMain.appendChild(launchLabel);
+            this.launches[launch.id] = launch;
+        }
       });
     })
     .catch((error) => {
@@ -93,24 +119,19 @@ Calendar.prototype.fetchMonthEvents = function(month, year) {
     })
 }
 
-
-// var tbl = document.getElementById('eTable');  //cache the table selector
-//
-// while (len--) {
-//  tbl.querySelector("tr[data-empid=" + idArray[len] + "]").style.display='none';
-// }
-
-
-
 Calendar.prototype.updateMonth = function(month, year) {
   const el = document.getElementById('calendar');
   const currentMonth = month + 1;
+  const nextMonth = (currentMonth === 12) ? 1 : month + 2;
+  const nextMonthYear = (currentMonth === 12) ? year + 1 : year;
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December'];
-  const febDays = ((year%100!=0) && (year%4==0) || (year%400==0)) ? 28 : 29;
+  const febDays = ((year%100!=0) && (year%4==0) || (year%400==0)) ? 29 : 28;
   const daysInMonth = [31, febDays, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   const currentMonthDays = daysInMonth[month];
   const currentMonthFirstWeekday = new Date(`${currentMonth} 1 ${year}`).getDay();
-  const nextMonthFirstWeekday = new Date(`${currentMonth + 1} 1 ${year}`).getDay();
+  const nextMonthFirstWeekday = new Date(`${nextMonth} 1 ${nextMonthYear}`).getDay();
+
+
   let previousMonthDays = daysInMonth[month - 1];
   const titleHtml = `${months[month]} - ${year}`;
   let count = 0;
@@ -124,23 +145,29 @@ Calendar.prototype.updateMonth = function(month, year) {
 
   calendarDaysHtml = `<div class="week">`;
 
+  // UI previous months days
   for (let i = 0; i < currentMonthFirstWeekday; i++) {
     count++;
     diff = currentMonthFirstWeekday - i - 1;
     day = previousMonthDays - diff;
-    calendarDaysHtml += `<div class="previousMonthDay" data-day="${day}">${day}</div>`;
+    calendarDaysHtml += `
+      <div class="previousMonthDay" data-day="${day}">
+        ${day}
+      </div>
+    `;
   }
-
+  // UI current months days
   for (let i = 0; i < currentMonthDays; i++) {
     count++;
-    day = (i !== 0 ) ? i + 1 : months[currentMonth].substring(0,3) + ' ' + i + 1;
+    day = (i !== 0 ) ? i + 1 : months[month].substring(0,3) + ' ' + i + 1;
 
     calendarDaysHtml += `
       <div class="currentMonthDay" data-day="${day}" data-month="${currentMonth}" data-year="${year}">
         <header>${day}</header>
         <main></main>
-      </div>`;
-
+      </div>
+    `;
+    // End of Week
     if (count % 7 === 0){
       calendarDaysHtml += `
         </div>
@@ -148,40 +175,26 @@ Calendar.prototype.updateMonth = function(month, year) {
       `;
     }
   }
-
-  for (let i = 0; i < 7 - nextMonthFirstWeekday; i++) {
-    count++;
-    day = i + 1;
-    calendarDaysHtml += `<div class="nextMonthDay" data-day="${day}" data-month="${currentMonth + 1}" data-year="${year}">${day}</div>`;
+  // UI next months days
+  if (nextMonthFirstWeekday !== 0) {
+    for (let i = 0; i < 7 - nextMonthFirstWeekday; i++) {
+      count++;
+      day = i + 1;
+      calendarDaysHtml += `
+        <div class="nextMonthDay" data-day="${day}" data-month="${currentMonth + 1}" data-year="${year}">
+          ${day}
+        </div>
+      `;
+    }
   }
-
   calendarDaysHtml += `</div>`;
+  const calTitle = document.getElementById('calendar-label');
+  const calBody = document.getElementById('calendar-body');
 
-  var calTitle = document.getElementById('calendar-label');
-  var calBody = document.getElementById('calendar-body');
-
+  // UI current Month - DOM
   calTitle.innerHTML = titleHtml;
   calBody.innerHTML = calendarDaysHtml;
+
+  // Fetch events for current Month
   this.fetchMonthEvents(currentMonth, year);
-
-  el.addEventListener('mouseover', (e) => {
-    const eventSourceData = e.target.dataset;
-    const dataKey = `${eventSourceData.day}/${eventSourceData.month}/${eventSourceData.year}`;
-
-    if (this.launches[dataKey]) {
-      console.log(this.launches[dataKey]);
-      alert(this.launches[dataKey].name);
-    }
-
-  });
-  el.addEventListener('mouseout', (e) => {
-    const eventSourceData = e.target.dataset;
-    const dataKey = `${eventSourceData.day}/${eventSourceData.month}/${eventSourceData.year}`;
-    if (this.launches[dataKey]) {
-
-    }
-  });
-  console.log(this.launches);
 }
-
-var calendar = new Calendar();
